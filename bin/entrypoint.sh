a48892f7caa6bf6b64054d753d26b1ad31df780d
Douglas Willcocks <douglas@d-tw.org>
2020-03-25 13:22:18 +0100

    docs: improved documentation

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,8 +1,20 @@
 #! /bin/bash
 
+# If we're in a Github Action, the GITHUB_WORKSPACE variable
+# will be set, and corresponds to the directory mounted as a volume
+# in the Docker.
+#
+# Since we're likely to be working on the files in the GITHUB_WORKSPACE
+# we automatically change directories
 if [ -v GITHUB_WORKSPACE ]; then
     cd $GITHUB_WORKSPACE
     echo "Switching to Github Workspace: ${GITHUB_WORKSPACE}"
 fi
 
+# The $@ passes all of the arguments passed to ./entrypoint.sh
+# to the python script
+#
+# ./entrypoint.sh --key my.key --path myfile.toml
+#
+# -> python /app/read_toml.py --key my.key --path myfile.toml
 python /app/read_toml.py $@
